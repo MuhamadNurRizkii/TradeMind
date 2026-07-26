307583c10aea489cd2332c70de30ef188cc3dddb
Muhamad Nur Rizki <farianhab123@gmail.com>
2026-07-26 10:26:49 +0700

    setting route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,19 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Pricing from "@/components/Pricing";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect("/dashboard");
+  }
   return (
     <main className="w-full flex flex-col flex-1">
       <Navbar />
